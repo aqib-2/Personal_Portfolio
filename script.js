4bb547d64e8fd398cb5794187aa7ca54e8b004c0
aqib-2 <maqib82001@gmail.com>
2023-05-16 12:17:47 +0530

    Updated CSS file
    fixed scroll bar for chrome

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const ham=document.getElementById('ham');
+const ham=document.getElementById("ham");
 const menu=document.getElementById("menu");
 const close=document.getElementById("close");
 const list=document.getElementById("list");
 const circular=document.getElementById("circletext");
    
-ham.addEventListener('click',()=>{
+ham.addEventListener('click', ()=>{
     if(menu.classList.contains('block')){
         menu.classList.replace('block','hidden');
         close.classList.replace('hidden','block');
         list.classList.replace('hidden','flex');
-        } else{
-            menu.classList.replace('hidden','block');
-            close.classList.replace('block','hidden');
-            list.classList.replace('flex','hidden');
-        }       
+    } else{
+        menu.classList.replace('hidden','block');
+        close.classList.replace('block','hidden');
+        list.classList.replace('flex','hidden');
+    }       
 })
 
 function circularText(){
